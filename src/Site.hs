4b54a608d6b186d35cfa7615d434d730c660c035
Eric Rochester <err8n@virginia.edu>
2011-08-19 12:41:03 -0400

    snap init

---
+++ src/Site.hs
@@ -0,0 +1,57 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+{-|
+
+This is where all the routes and handlers are defined for your site. The
+'site' function combines everything together and is exported by this module.
+
+-}
+
+module Site
+  ( site
+  ) where
+
+import           Control.Applicative
+import           Data.Maybe
+import qualified Data.Text.Encoding as T
+import           Snap.Extension.Heist
+import           Snap.Extension.Timer
+import           Snap.Util.FileServe
+import           Snap.Types
+import           Text.Templating.Heist
+
+import           Application
+
+
+------------------------------------------------------------------------------
+-- | Renders the front page of the sample site.
+--
+-- The 'ifTop' is required to limit this to the top of a route.
+-- Otherwise, the way the route table is currently set up, this action
+-- would be given every request.
+index :: Application ()
+index = ifTop $ heistLocal (bindSplices indexSplices) $ render "index"
+  where
+    indexSplices =
+        [ ("start-time",   startTimeSplice)
+        , ("current-time", currentTimeSplice)
+        ]
+
+
+------------------------------------------------------------------------------
+-- | Renders the echo page.
+echo :: Application ()
+echo = do
+    message <- decodedParam "stuff"
+    heistLocal (bindString "message" (T.decodeUtf8 message)) $ render "echo"
+  where
+    decodedParam p = fromMaybe "" <$> getParam p
+
+
+------------------------------------------------------------------------------
+-- | The main entry point handler.
+site :: Application ()
+site = route [ ("/",            index)
+             , ("/echo/:stuff", echo)
+             ]
+       <|> serveDirectory "resources/static"
